260eeda277bc260f904c9412dacf35e67f4f65cc
IliaKuryndin <6apsyk@mail.ru>
2022-08-31 18:03:30 +0300

    jest

--- src/shared/Dropdown/__tests__/Dropdown.test.tsx
+++ src/shared/Dropdown/__tests__/Dropdown.test.tsx
@@ -5,7 +5,7 @@ import { Dropdown } from "../Dropdown";
 describe("Dropdown", () => {
     test("should render", () => {
         const wrapper = shallow(<Dropdown children={<div />} button={<button />} />);
-        // // expect(wrapper).toBeDefined();
-        // // expect(wrapper.find("#button")).toBeDefined();
+        expect(wrapper).toBeDefined();
+        expect(wrapper.find("#button")).toBeDefined();
     });
 });
